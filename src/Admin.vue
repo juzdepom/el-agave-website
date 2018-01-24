c30c02d0e1f42d9cd03eb0f5610c0f413dac0544
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-01-23 23:04:01 -0800

    Admin is work in progress.

--- src/Admin.vue
+++ src/Admin.vue
@@ -1,30 +1,60 @@
 <template>
     <div class="container-fluid admin-margins " id="admin">
-        <!-- back button -->
-        <a
-            href="#app"
-            class="btn btn-warning back"
-            @click="backToMain()">
-            <span class="fa fa-chevron-left"></span>
-            &nbsp;&nbsp;Back to Main Menu (don't save)
-        </a>
-        <!-- back button -->
-        <div class="container-fluid text-center admin-margins card-style">
-            <a
-                href="#app"
-                class="btn btn-success pull-right"
-                >
-                Save Changes
-            </a><br><br>
+        <div class="header-main"
+            style="background-color: white; border-bottom: 1px solid lightgray; height: 55px" >
+            <div class="container">
+                <nav class="navbar navbar-default navbar-expand-sm">
+                    <div class="navbar-header">
+                        <nav-collapse-button-component></nav-collapse-button-component>
+                    </div>
+                        <a
+                            href="#app"
+                            class="btn btn-warning back pull-left"
+                            @click="backToMain()">
+                            <span class="fa fa-chevron-left"></span>
+                            &nbsp;&nbsp;Back to Main Menu (don't save)
+                        </a>
+                        <a
+                            href="#app"
+                            class="btn btn-success pull-right"
+                            >
+                            Save Changes
+                        </a>
+                </nav>
+            </div>
+        </div>
+        <div class="container-fluid text-center admin-margins form-group">
+            <br><br>
             <h1>Welcome to the El Agave Admin!</h1>
-            <h2>Open Hours</h2>
-            <h2>Menu</h2>
-            <h2>About Us</h2>
-            <h2>Locations</h2>
-            <h2>We Support Our Students</h2>
-            <h2>Yelp Reviews</h2>
-            <h2>Deals</h2>
 
+            <h2>Open Hours</h2>
+            <i>Mini coding tutorial: Including <span style="color: red;">&lt;br&gt;</span> will create a new line.</i>
+            <br>
+            <textarea class="form-control" ></textarea>
+
+            <h2>Menu</h2>
+            <i>Mini coding tutorial: the menu below is written in JSON, a popular way of storing data on servers.<br>
+            To add a new menu item, write { title: "Nachos", redText: "Description of Nachos", price: 9.95 },
+            </i>
+            <br>
+            <textarea class="form-control"></textarea>
+            <h2>About Us</h2>
+            <textarea class="form-control"></textarea>
+            <h2>Locations</h2>
+            <!-- <div> -->
+                <textarea class="form-control"></textarea><br>
+                <textarea class="form-control"></textarea><br>
+                <textarea class="form-control"></textarea><br>
+            <!-- </div> -->
+
+            <h2>We Support Our Students</h2>
+            <textarea class="form-control"></textarea>
+            <h2>Yelp Reviews</h2>
+                <div style="width: 50%; margin-left: 25%" >
+                    <i class="" style="width: 50%; ">For alterations please email <span style="color: blue">julia.gao.miller@gmail.com:</span> title, description, submission date, author, author image. </i>
+                </div>
+            <h2>Deals</h2>
+            <textarea class="form-control"></textarea>
         </div>
 
     </div>
@@ -41,6 +71,22 @@ export default {
 </script>
 
 <style>
+
+    .admin-margins a {
+        margin-top: 10px;
+    }
+
+    .admin-margins h2 {
+        margin-top: 20px;
+    }
+    textarea {
+        width: 500px;
+        margin-top: 10px;
+        /* margin-bottom: 20px; */
+    }
+    .margin-ten {
+        margin-top: 30px;
+    }
     .card-style {
         border: 1px solid #ccc;
         box-shadow: 0px 3px 6px #ccc;
@@ -48,6 +94,7 @@ export default {
     }
     .admin-margins {
         margin: 20px 0 0 10px;
+        padding-bottom: 10px;
     }
 </style>
 
