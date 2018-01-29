8f6f99fbd6367492b843c688987e32c972d44dc1
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-01-29 11:25:51 -0800

    removed some unnecessary code

--- src/main.js
+++ src/main.js
@@ -1,15 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
-import routes from './routes'
 import store from './store'
 
 Vue.use(VueResource)
 
 new Vue({
   el: '#app',
-  routes,
   store,
   render: h => h(App)
 })
